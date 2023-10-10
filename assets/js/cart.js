d5f193f923aafe95231cd87fca3c4fcf1b7c3a05
Thewebguyy <oliubode2002@gmail.com>
2023-10-10 16:14:49 +0100

    updated pictures

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,74 +1,57 @@
-// Create an empty cart array
-var cart = [];
+// Create a new shopping cart object.
+const shoppingCart = new ShoppingCart();
 
-// Add an event listener to all "Add to Cart" buttons
+// Add an event listener to all "Add to Cart" buttons.
 document.querySelectorAll('.menu-button').forEach(function(button) {
   button.addEventListener('click', function(event) {
-    // Get the item name and price from the button attributes
-    var itemName = this.getAttribute('data-item-name');
-    var itemPrice = this.getAttribute('data-item-price');
+    // Get the item name and price from the button attributes.
+    const itemName = this.getAttribute('data-item-name');
+    const itemPrice = this.getAttribute('data-item-price');
 
-    // Create a new cart item object
-    var cartItem = {
+    // Create a new cart item object.
+    const cartItem = {
       name: itemName,
       price: itemPrice,
       quantity: 1
     };
 
-    // Add the cart item to the cart array
-    cart.push(cartItem);
+    // Add the cart item to the shopping cart.
+    shoppingCart.addItem(cartItem);
 
-    // Update the cart display
+    // Update the cart display.
     updateCartDisplay();
-
-    // Add a "Remove from Cart" button to the cart item
-    var removeButton = document.createElement('button');
-    removeButton.innerHTML = 'Remove from Cart';
-    removeButton.addEventListener('click', function() {
-      removeItemFromCart(itemName);
-    });
-    cartItem.element.appendChild(removeButton);
   });
 });
 
-// Remove items from the cart
+// Remove items from the shopping cart.
 function removeItemFromCart(itemName) {
-  // Find the cart item in the cart array
-  var cartItem = cart.find(function(item) {
-    return item.name === itemName;
-  });
+  shoppingCart.removeItem(itemName);
 
-  // Remove the cart item from the cart array
-  cart.splice(cart.indexOf(cartItem), 1);
-
-  // Update the cart display
+  // Update the cart display.
   updateCartDisplay();
 }
 
-// Change the quantity of items in the cart
+// Change the quantity of items in the cart.
 function changeItemQuantityInCart(itemName, quantity) {
-  // Find the cart item in the cart array
-  var cartItem = cart.find(function(item) {
-    return item.name === itemName;
-  });
+  shoppingCart.changeItemQuantity(itemName, quantity);
 
-  // Update the cart item quantity
-  cartItem.quantity = quantity;
-
-  // Update the cart display
+  // Update the cart display.
   updateCartDisplay();
 }
 
 // Checkout
 function checkout() {
-  // TODO: Implement checkout functionality
+  // Get the total price of the shopping cart.
+  const totalPrice = shoppingCart.getTotalPrice();
+
+  // TODO: Implement checkout functionality using the total price.
 }
 
-// Update the cart display
+// Update the cart display.
 function updateCartDisplay() {
-  // Get the number of items in the cart
-  var numItemsInCart = cart.length;
+  // Get the number of items in the shopping cart.
+  const numItemsInCart = shoppingCart.items.length;
 
-  // Update the cart display element
-  document.getElementById('cart-display').innerHTML = 'Cart contains ' + numItemsInCart + ' items';
+  // Update the cart display element using the textContent property.
+  document.getElementById('cart-display').textContent = 'Cart contains ' + numItemsInCart + ' items';
 }
