ff0d47196c667c73c876506b11b6bde2a2ea02c5
Olabode <olabodewebdesigns02@gmail.com>
2023-10-13 14:46:07 +0100

    working on the menu

--- js/cart.js
+++ js/cart.js
@@ -1,39 +1,51 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Select all "Add to Cart" buttons
-    const addToCartButtons = document.querySelectorAll(".menu-button");
+// cart.js
+import { items } from './sharedData';
 
-    // Add a click event listener to each button
-    addToCartButtons.forEach(button => {
-        button.addEventListener("click", function() {
-            const itemName = this.getAttribute("data-item-name");
-            const itemPrice = parseFloat(this.getAttribute("data-item-price"));
-            const itemQuantity = 1; // You can customize this based on your requirements
+document.addEventListener("DOMContentLoaded", function () {
+    // Function to render cart items
+    function renderCartItems() {
+        const cartItemsContainer = document.getElementById("cart-items");
+        cartItemsContainer.innerHTML = "";
 
-            // Create an object to represent the cart item
-            const cartItem = { name: itemName, price: itemPrice, quantity: itemQuantity };
+        for (let i = 0; i < items.length; i++) {
+            const item = items[i];
+            const row = document.createElement("tr");
+            row.innerHTML = `
+                <td class="image" data-title="No">
+                    <img src="${item.image}" alt="${item.name}">
+                </td>
+                <td class="product-des" data-title="Description">
+                    <p class="product-name"><a href="#">${item.name}</a></p>
+                    <p class="product-des">${item.description}</p>
+                </td>
+                <td class="price" data-title="Price"><span>#${item.price}</span></td>
+                <td class="qty" data-title="Qty">
+                    <div class="input-group">
+                        <div class="button minus">
+                            <button type="button" class="btn btn-primary btn-number" disabled="disabled" data-type="minus" data-field="quant[${i}]">
+                                <i class="ti-minus"></i>
+                            </button>
+                        </div>
+                        <input type="text" name="quant[${i}]" class="input-number" data-min="1" data-max="1000" value="${item.quantity || 0}">
+                        <div class="button plus">
+                            <button type="button" class="btn btn-primary btn-number" data-type="plus" data-field="quant[${i}]">
+                                <i class="ti-plus"></i>
+                            </button>
+                        </div>
+                    </div>
+                </td>
+                <td class="total-amount" data-title="Total"><span>#${(item.price * (item.quantity || 0))}</span></td>
+                <td class="action" data-title="Remove">
+                    <a href="#" class="remove-from-cart" data-index="${i}">
+                        <i class="ti-trash remove-icon"></i>
+                    </a>
+                </td>
+            `;
 
-            // Check if the cart already exists in localStorage
-            let cart = JSON.parse(localStorage.getItem("cart")) || [];
+            cartItemsContainer.appendChild(row);
+        }
+    }
 
-            // Check if the item is already in the cart
-            const existingItemIndex = cart.findIndex(item => item.name === itemName);
-
-            if (existingItemIndex !== -1) {
-                // If the item is already in the cart, update the quantity
-                cart[existingItemIndex].quantity += itemQuantity;
-            } else {
-                // If the item is not in the cart, add it
-                cart.push(cartItem);
-            }
-
-            // Update the cart in localStorage
-            localStorage.setItem("cart", JSON.stringify(cart));
-
-            // Provide feedback to the user (you can customize this)
-            alert(`${itemName} added to the cart!`);
-
-            // You can optionally redirect to the cart page here if needed
-            // window.location.href = "cart.html";
-        });
-    });
+    // Call the renderCartItems function to populate the cart
+    renderCartItems();
 });
---
+++ js/sharedData.js
@@ -0,0 +1,26 @@
+// sharedData.js
+const items = [
+    {
+        image: "path/to/image1.jpg",
+        name: "Afredo Pasta",
+        description: "A creamy delight...",
+        price: 5000,
+        quantity: 1,
+    },
+    {
+        image: "path/to/image2.jpg",
+        name: "Seafood Pasta",
+        description: "A luxurious blend...",
+        price: 6000,
+        quantity: 2,
+    },
+    {
+        image: "path/to/image3.jpg",
+        name: "RedSauce Pasta",
+        description: "A classic dish...",
+        price: 5000,
+        quantity: 3,
+    },
+];
+
+export { items };
